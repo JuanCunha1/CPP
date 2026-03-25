85d119c298e49889620f2bf5b3822e22e10dc9ad
Juan Cunha <juan.gucunha@gmail.com>
2026-03-25 14:16:58 +0100

    change makefiles

--- Module_1/ex04/main.cpp
+++ Module_1/ex04/main.cpp
@@ -1,39 +1,42 @@
 #include <iostream>
 #include <fstream>
+#include <string>
+
+void	replace(std::string line, std::string fileName, std::string replaceString, std::string replacement) {
+	size_t pos = 0;
+	std::ofstream fileReplace;
+
+	fileName += ".replace";
+	fileReplace.open(fileName.c_str(), std::ios::out);
+	if (!fileReplace.is_open()) {
+		std::cerr << "Error creating output file\n";
+		return ;
+	}
+	while ((pos = line.find(replaceString, pos)) != std::string::npos) {
+		line.erase(pos, replaceString.length());
+		line.insert(pos, replacement);
+		pos += replacement.length();
+	}
+	fileReplace << line;
+	fileReplace.close();
+}
 
 int main(int argc, char **argv) {
 	if (argc != 4) {
-		std::cout << "Not enough arguments" << std::endl;
+		std::cout << "Incorrect input\n./sed_for_loser <filename> <string1> <string2>" << std::endl;
 		return (1);
 	}
 	std::string line;
-	std::ifstream file_replace;
-	std::ofstream file;
+	std::string fileLine;
+	std::ifstream file;
 	file.open(argv[1], std::ios::in);
 	if (file.fail()) {
 		std::cerr << "Error opening file\n";
 		return 1;
 	}
-	std::string file_replace_name = argv[1];
-	file_replace_name += ".replace";
-	file_replace.open(file_replace_name, std::ios::out);
-	if (!file_replace.is_open()) {
-        std::cerr << "Error creating output file\n";
-        return 1;
-    }
-	while ()
-
-	std::string brainString = "HI THIS IS BRAIN";
-	std::string *stringPTR = &brainString;
-	std::string &stringREF = brainString;
-
-	std::cout << &brainString << std::endl;
-	std::cout << stringPTR << std::endl;
-	std::cout << &stringREF << std::endl;
-
-	std::cout << brainString << std::endl;
-	std::cout << *stringPTR << std::endl;
-	std::cout << stringREF << std::endl;
-
+	while (getline(file, fileLine))
+		line += fileLine + "\n";
+	file.close();
+	replace(line, argv[1], argv[2], argv[3]);
 	return (0);
 }
