9db408796bbe9cbbb714cdcce6e1fc03de06b002
Juan Cunha <juan.gucunha@gmail.com>
2026-03-31 11:34:26 +0200

    add tab

--- Module_1/ex00/main.cpp
+++ Module_1/ex00/main.cpp
@@ -1,9 +1,9 @@
 #include "Zombie.hpp"
 
 int main(void) {
-    Zombie *Zombie1 = newZombie("Zombienn");
-    Zombie1->announce();
-    randomChump("me mario");
-    delete Zombie1;
+	Zombie *Zombie1 = newZombie("Zombienn");
+	Zombie1->announce();
+	randomChump("me mario");
+	delete Zombie1;
 	return (0);
 }
