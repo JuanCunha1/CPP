de02133ac2a6fd37c92f283bbcd1b8a0ff3ae1ae
Juan Cunha <juan.gucunha@gmail.com>
2026-03-31 12:07:25 +0200

    change std::endl to \n in some prints

--- Module_1/ex06/Harl.cpp
+++ Module_1/ex06/Harl.cpp
@@ -2,24 +2,24 @@
 #include <string>
 
 void Harl::debug() {
-	std::cout << "[ DEBUG ]" << std::endl;
+	std::cout << "[ DEBUG ]\n";
 	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!\n" << std::endl;
 }
 
 void Harl::info() {
-	std::cout << "[ INFO ]" << std::endl;
+	std::cout << "[ INFO ]\n";
 	std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put"
 				 "enough bacon in my burger! If you did, I wouldn’t be asking for more!\n" << std::endl;
 }
 
 void Harl::warning() {
-	std::cout << "[ WARNING ]" << std::endl;
+	std::cout << "[ WARNING ]\n";
 	std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for "
 	"years, whereas you started working here just last month.\n" << std::endl;
 }
 
 void Harl::error() {
-	std::cout << "[ ERROR ]" << std::endl;
+	std::cout << "[ ERROR ]\n";
 	std::cout << "This is unacceptable! I want to speak to the manager now.\n" << std::endl;
 }
 
@@ -30,17 +30,13 @@ void Harl::complain( std::string str ) {
 	std::string levels[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
 	int i;
 	for (i = 0; i <= 3; i++) {
-        if (i == 3 && level != levels[i]) {
-            i = -1;
-            break;
-        }
-        if (level == levels[i])
-            break;
+		if (level == levels[i])
+			break ;
     }
 	switch (i) {
 	 	default:
             std::cout << "[ Probably complaining about insignificant problems ]\n";
-            break;
+            break ;
         case 0:
             (this->*func_ptr[0])();
             // fall through
